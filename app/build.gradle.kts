2f61196801c4b9fea825cad35cda83d742ade1f1
kenkoro <frogharvard@gmail.com>
2024-06-21 19:59:29 +0700

    fix: app's version name to 0.1.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
     minSdk = 24
     targetSdk = 34
     versionCode = 1
-    versionName = "0.1.0-mvp"
+    versionName = "0.1.14"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
