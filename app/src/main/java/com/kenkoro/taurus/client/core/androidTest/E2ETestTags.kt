01881c547a4489d4d8a0e01aa11e9e409ee2bb15
kenkoro <frogharvard@gmail.com>
2024-09-08 12:37:52 +0700

    chore: unused test tags

--- app/src/main/java/com/kenkoro/taurus/client/core/androidTest/E2ETestTags.kt
+++
@@ -1,6 +0,0 @@
-package com.kenkoro.taurus.client.core.androidTest
-
-object E2ETestTags {
-  const val SUBJECT_TEXT_FIELD = "SUBJECT_TEXT_FIELD"
-  const val PASSWORD_TEXT_FIELD = "PASSWORD_TEXT_FIELD"
-}
